54cfd73a416cc5e8979290c6c101b7a21da01d67
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-05-31 22:19:07 +0300

    Remove db; add views

--- sinatra-app/server.rb
+++ sinatra-app/server.rb
@@ -1,13 +1,6 @@
 require 'sinatra'
-require './user_db'
+require 'slim'
 
 get '/' do
-  slim 'img src="https://i.pinimg.com/originals/85/a0/ed/85a0edc55fff61e462e47b60bb2f5ee0.jpg"'
-end
-
-get '/hello/:name' do
-  name = params[:name]
-  DB.insert(name: name, age: rand(1..100))
-  slim 'p insert name in db'
-  slim "p your age is #{DB.last.age}"
+  slim :'hello/index'
 end
